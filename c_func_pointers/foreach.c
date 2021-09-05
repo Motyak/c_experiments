100d479d519ce5db515cb55c9e7e2e63f4ccef90
Tommy Anfosso <anfossotommy@hotmail.fr>
2021-09-05 15:43:16 +0200

    changed c foreach

--- c_func_pointers/foreach.c
+++ c_func_pointers/foreach.c
@@ -1,6 +1,6 @@
 #include "stdio.h"
 
-void foreach(void* elements, int length, int size, void (*fn)(void*))
+void foreach(void* elements, int length, size_t size, void (*fn)(void*))
 {
     for(int i = 0 ; i < length ; ++i)
         fn(elements + i * size);
@@ -19,11 +19,11 @@ int main()
     int strLength = sizeof(str) / sizeof(char);
 
     foreach(arr, arrLength, sizeof(int), square);
-    foreach(str, strLength, sizeof(char), upper);
+    foreach(str, strLength - 1, sizeof(char), upper);
 
     for(int i = 0 ; i < arrLength ; ++i)
         printf("%d\n", arr[i]);
-    for(int i = 0 ; i < strLength ; ++i)
+    for(int i = 0 ; i < strLength - 1 ; ++i)
         printf("%c", str[i]);
     printf("\n");
 }
