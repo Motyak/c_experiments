f77b33fc4f24a7d0baf53c208c10e71358cf2fb8
Tommy Anfosso <anfossotommy@hotmail.fr>
2021-12-28 13:09:55 +0100

    added c map

---
+++ c_map/c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum Type { NAME, NICKNAME };
+
+int main()
+{
+
+    char str1[] = "tommy";
+    char* str2 = "motyak";
+
+    char** map = malloc(2 * sizeof(str2));
+    *map = str1;
+    *(map + 1) = str2;
+
+    printf("%s,%s\n", map[NAME], map[NICKNAME]);
+
+    free(map);
+}
